4a46710940a1d93177c3b07261ad6132eea6047d
DorianLudm <122395205+DorianLudm@users.noreply.github.com>
2023-05-22 11:28:27 +0200

    Add files via upload

--- src/Connexion.java
+++ src/Connexion.java
@@ -70,12 +70,14 @@ public class Connexion extends Application{
                 "-fx-border-width: 5em;" +
                 "-fx-border-radius:50%;"
         );
+        VBox box = new VBox();
         Button connexion = new Button("Démarrer!");
         connexion.setOnAction(new GestionConnexion(this));
-
+        box.getChildren().add(connexion);
+        box.setAlignment(Pos.CENTER);
         //Ajout des éléments à root
         root.add(user,0,0);
-        root.add(connexion,0,1);
+        root.add(box,0,1);
         root.setAlignment(Pos.CENTER);
         //Ajout de la GridPane dans l'attribut mainPane
         this.mainPane = root;
@@ -132,6 +134,62 @@ public class Connexion extends Application{
         this.afficherFenetre();
     }
 
+    // public void inscription(){
+    //     GridPane root = new GridPane();
+    //     BackgroundImage backGround = new BackgroundImage(new Image("file:img/BackgroundWithLogo.png",400,650,false,true), null, null, null, null);
+    //     root.setBackground(new Background(backGround));
+
+    //     VBox mainBox = new VBox();
+    //     BackgroundImage backGround2 = new BackgroundImage(new Image("file:img/BgInscription.png",254,400,false,true), null, null, null, null);
+    //     mainBox.setBackground(new Background(backGround2));
+    //     mainBox.setPrefWidth(254);
+    //     mainBox.setPrefHeight(400);
+    //     //Username
+    //     Label username = new Label("Nom d'utilisateur");
+    //     username.setFont(Font.font("Arial",FontWeight.BOLD,18));
+    //     username.setTextFill(Color.web("#ffffff"));
+    //     VBox.setMargin(username, new Insets(120, 40, 5, 10));
+    //     TextField tF = new TextField();
+    //     VBox.setMargin(tF, new Insets(0, 40, 10, 10));
+    //     //PassWord
+    //     Label password = new Label("Mot de passe");
+    //     password.setFont(Font.font("Arial",FontWeight.BOLD,18));
+    //     password.setTextFill(Color.web("#ffffff"));
+    //     VBox.setMargin(password, new Insets(0, 40, 5, 10));
+    //     PasswordField pwF = new PasswordField();
+    //     VBox.setMargin(pwF, new Insets(0, 40, 10, 10));
+    //     //Mail
+    //     Label mail = new Label("Adresse Mail");
+    //     mail.setFont(Font.font("Arial",FontWeight.BOLD,18));
+    //     mail.setTextFill(Color.web("#ffffff"));
+    //     VBox.setMargin(mail, new Insets(0, 40, 5, 10));
+    //     TextField tfMail = new TextField();
+    //     VBox.setMargin(tfMail, new Insets(0, 40, 10, 10));
+    //     //Buttons
+    //     HBox box = new HBox();
+    //     Button connexion = new Button("Connexion");
+    //     connexion.setOnAction(new GestionConnexion(this));
+    //     HBox.setMargin(connexion, new Insets(1));
+    //     Button inscription = new Button("Inscrivez-vous!");
+    //     inscription.setOnAction(new GestionConnexion(this));
+    //     HBox.setMargin(inscription, new Insets(1));
+    //     box.getChildren().addAll(inscription,connexion);
+    //     VBox.setMargin(box, new Insets(0, 35, 10, 10));
+
+    //     mainBox.getChildren().addAll(username,tF,password,pwF,mail,tfMail,box);
+        
+
+    //     //Ajout des éléments à root
+    //     root.add(mainBox,0,0);
+    //     root.setAlignment(Pos.CENTER);
+
+    //     //Ajout de la GridPane dans l'attribut mainPane
+    //     this.mainPane = root;
+
+    //     //Affichage de la fenêtre
+    //     this.afficherFenetre();
+    // }
+
     public void inscription(){
         GridPane root = new GridPane();
         BackgroundImage backGround = new BackgroundImage(new Image("file:img/BackgroundWithLogo.png",400,650,false,true), null, null, null, null);
@@ -163,18 +221,25 @@ public class Connexion extends Application{
         VBox.setMargin(mail, new Insets(0, 40, 5, 10));
         TextField tfMail = new TextField();
         VBox.setMargin(tfMail, new Insets(0, 40, 10, 10));
-        //Buttons
-        HBox box = new HBox();
-        Button connexion = new Button("Connexion");
-        connexion.setOnAction(new GestionConnexion(this));
-        HBox.setMargin(connexion, new Insets(1));
-        Button inscription = new Button("Inscrivez-vous!");
+        //Button
+        Button inscription = new Button("Inscription");
         inscription.setOnAction(new GestionConnexion(this));
-        HBox.setMargin(inscription, new Insets(1));
-        box.getChildren().addAll(inscription,connexion);
-        VBox.setMargin(box, new Insets(0, 35, 10, 10));
+        VBox.setMargin(inscription, new Insets(0, 40, 10, 10));
+        //Lower
+        Label text = new Label("Vous avez déjà un compte ?");
+        text.setFont(Font.font("Arial",FontWeight.BOLD,12));
+        VBox.setMargin(text, new Insets(0, 40, 2, 10));
+        Button connexion = new Button("Connectez-vous!");
+        connexion.setOnAction(new GestionConnexion(this));
+        VBox.setMargin(connexion, new Insets(0, 40, 10, 10));
+        //Box
+        // HBox box = new HBox();
+        
+        // HBox.setMargin(inscription, new Insets(1));
+        // box.getChildren().addAll(inscription,connexion);
+        // VBox.setMargin(box, new Insets(0, 35, 10, 10));
 
-        mainBox.getChildren().addAll(username,tF,password,pwF,mail,tfMail,box);
+        mainBox.getChildren().addAll(username,tF,password,pwF,mail,tfMail,inscription,text,connexion);
         
 
         //Ajout des éléments à root
