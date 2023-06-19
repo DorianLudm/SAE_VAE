71ac3773832b0160d23d18e6e46e438dc047a461
CassandraM <cassou.maupou@gmail.com>
2023-06-19 11:19:24 +0200

    Classes Photo V1

--- Code/src/Photo.java
+++ Code/src/Photo.java
@@ -1,6 +1,50 @@
+import java.io.File;
+
 public class Photo {
     private int id;
     private String titre;
+    private File img;
+    private Objet objet;
 
+   public Photo(int id, String titre, Objet obj){
+        this.id = id;
+        this.titre = titre;
+        this.objet = obj;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
     
+    public File getImg(){
+        return this.img;
+    }
+    
+    public String getURLImg() {
+        return img.getAbsolutePath();
+    }
+
+    public void setImg(String url) {
+        this.img = new File(url);
+    }
+
+    public Objet getObjet() {
+        return objet;
+    }
+
+    public void setObjet(Objet objet) {
+        this.objet = objet;
+    }
 }
--- Code/src/PhotoBD.java
+++ Code/src/PhotoBD.java
@@ -1,3 +1,47 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PhotoBD {
-    
+    private ConnexionBD laConnexion;
+    private Statement st;
+
+    public PhotoBD(ConnexionBD laConnexion){
+        this.laConnexion = laConnexion;
+    }
+
+    public int maxNumPhoto() throws SQLException{
+        this.st = this.laConnexion.createStatement();
+        ResultSet rs = this.st.executeQuery("select count(idph) from PHOTO");
+        rs.next();
+        int val = rs.getInt(1);
+        return val;
+    }
+
+    public int insererPhotoBD(Photo photo) throws SQLException{
+        photo.setId(maxNumPhoto()+1);
+        PreparedStatement s = this.laConnexion.prepareStatement("insert into PHOTO values (?,?,?,?)");
+        s.setInt(1, photo.getId());
+        s.setString(2, photo.getTitre());
+        s.setString(3, photo.getURLImg());
+        s.setInt(4, photo.getObjet().getId());
+        s.executeUpdate();
+        return photo.getId();
+    }
+
+    public void effacerPhotoBD(int id) throws SQLException{
+        PreparedStatement s = this.laConnexion.prepareStatement("delete from PHOTO where idph=?");
+        s.setInt(1, id);
+        s.executeUpdate();
+    }
+
+    public void majPhotoBD(Photo photo) throws SQLException{
+        PreparedStatement s = this.laConnexion.prepareStatement("update PHOTO SET idph=?, titreph=?, imgph=?, idob=?");
+        s.setInt(1, photo.getId());
+        s.setString(2, photo.getTitre());
+        s.setString(3, photo.getURLImg());
+        s.setInt(4, photo.getObjet().getId());
+        s.executeUpdate();
+    }
 }
