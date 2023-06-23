dd742adf286f680ab3ace08c56155b8c12567051
DorianLudm <122395205+DorianLudm@users.noreply.github.com>
2023-06-23 13:57:49 +0200

    Delete lancer.sh

--- lancer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-javac -d bin src/*.java --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls
-java -cp bin:/usr/share/java/mariadb-java-client.jar/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls AppliVAE
