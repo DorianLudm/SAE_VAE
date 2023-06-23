0abd12dd447754e3545711762e4811b5712728cb
CassandraM <cassou.maupou@gmail.com>
2023-06-23 13:40:03 +0200

    Docstring effectuées

--- Code/src/Role.java
+++ Code/src/Role.java
@@ -1,24 +1,48 @@
 public class Role{
+    /**L'identifiant du rôle */
     private int id;
+
+    /**Le nom du rôle */
     private String nom;
 
+    /**
+     * Constructeur de base de la classe Role
+     * @param id
+     * @param nom
+     */
     public Role(int id, String nom){
         this.id = id;
         this.nom = nom;
     }
 
+    /**
+     * 
+     * @return l'identifiant du rôle
+     */
     public int getId(){
         return this.id;
     }
 
+    /**
+     * 
+     * @return le nom du rôle
+     */
     public String getNom(){
         return this.nom;
     }
 
+    /**
+     * Permet de saisir l'identifiant du rôle
+     * @param id
+     */
     public void setId(int id){
         this.id = id;
     }
 
+    /**
+     * Permet de saisir le nom du rôle
+     * @param nom
+     */
     public void setNom(String nom){
         this.nom = nom;
     }
